2b236173398f64d7efd80dd2a18d86ed38842f1c
stevenshuhyow <stevenshuhyow@gmail.com>
2015-04-22 16:18:30 -0700

    Pass LinksController tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,12 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', ['$scope', '$http', function ($scope, $http, Links) {
   // Your code here
-});
+  $scope.data = {};
+  $scope.getLinks = function(){
+    $http.get('/api/links').success(function(data) {
+      $scope.data.links = data;
+    });
+  };
+  $scope.getLinks();
+}]);
