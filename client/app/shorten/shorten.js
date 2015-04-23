a8a24aa4c6b87f5711ae590a5a08ff7f4904e4e8
stevenshuhyow <stevenshuhyow@gmail.com>
2015-04-22 17:02:18 -0700

    Pass ShortenController tests

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,11 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
-});
+.controller('ShortenController', ['$scope', '$http', '$location', 'Links', function ($scope, $http, $location, Links) {
+  $scope.link = {};
+  $scope.addLink = function(url) {
+    $http.post('/api/links', {url: url}).success(function(data) {
+      console.log('data returned from POST to /api/links',data);
+      // $scope.link
+    });
+  };
+}]);
