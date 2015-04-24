5966ef3004eed190491104e40989622d4f54a101
stevenshuhyow <stevenshuhyow@gmail.com>
2015-04-23 17:52:00 -0700

    Include authentication functionality and remove html5 pushState for URL paths

--- client/app/app.js
+++ client/app/app.js
@@ -17,17 +17,19 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     })
     .otherwise({
       redirectTo: '/links'
     });
     // Your code here
-    $locationProvider.html5Mode(true);
+    // $locationProvider.html5Mode(true);
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,6 @@
 var morgan      = require('morgan'), // used for logging incoming request
     bodyParser  = require('body-parser'),
-    helpers     = require('./helpers.js'), // our custom middleware
-    path        = require('path'),
-    Link        = require('../links/linkModel'),
-    linksController  = require('../links/linkController.js');
+    helpers     = require('./helpers.js'); // our custom middleware
 
 
 module.exports = function (app, express) {
@@ -19,7 +16,7 @@ module.exports = function (app, express) {
   app.use('/api/users', userRouter); // use user router for all user request
 
   // authentication middleware used to decode token and made available on the request
-  //app.use('/api/links', helpers.decode);
+  app.use('/api/links', helpers.decode);
   app.use('/api/links', linkRouter); // user link router for link request
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
@@ -27,44 +24,4 @@ module.exports = function (app, express) {
   // inject our routers into their respective route files
   require('../users/userRoutes.js')(userRouter);
   require('../links/linkRoutes.js')(linkRouter);
-
-  app.get('/favicon.ico', function(req, res) {
-    res.set('Content-Type', 'image/x-icon');
-    res.status(200).end();
-  });
-
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client/index.html'));
-  });
-
-
-  // app.param('code', linksController.findUrl);
-  // app.get('/:code', linksController.navToLink);
-
-
-  // app.get('/*', function(req, res) {
-  //   Link.findOne({code: req.params[0]}, function(error,link) {
-  //     if (!link) {
-  //       res.redirect('/');
-  //     } else {
-  //       link.visits++;
-  //       link.save(function(err) {
-  //         if(err){
-  //           helpers.errorHandler(err, req, res);
-  //         } else {
-  //           return res.redirect(link.url);
-  //         }
-  //       });
-  //     }
-  //   });
-  // });
-
-
-  // app.use('/*', function(req, res) {
-  //   res.redirect('/#' + req.baseUrl);
-  // });
-
-  // app.use('/#*', function(req, res) {
-  //   res.sendFile(path.join(__dirname, '../../client/index.html'));
-  // });
 };
